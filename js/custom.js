a110093594e7e1df8dc7d806936d1769d7f73714
Ronald Joseph <rex.rony@gmail.com>
2020-02-09 14:23:40 +0400

    backtop

--- js/custom.js
+++ js/custom.js
@@ -147,4 +147,17 @@ $(window).scroll(function() {
 
 });
 /*----------------------------------------------------Back to top---------------------------------------------------------------*/
-$(document).ready(function(){$("#back-top").hide();$(function(){$(window).scroll(function(){if($(this).scrollTop()>100){$("#back-top").fadeIn()}else{$("#back-top").fadeOut()}});$("#back-top a").click(function(){$("body,html").animate({scrollTop:0},800);return false})})})
+$(document).ready(function(){$("#back-top").hide();$(function(){$(window).scroll(function(){if($(this).scrollTop()>100){$("#back-top").fadeIn()}else{$("#back-top").fadeOut()}});$("#back-top a").click(function(){$("body,html").animate({scrollTop:0},800);return false})})});
+
+/****onclick scroll*****/
+$('a[href*=\\#]:not([href=\\#])').on('click', function() {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+  if (target.length) {
+      $('html,body').animate({
+          scrollTop: target.offset().top
+      }, 1000);
+      return false;
+  }
+});
+
